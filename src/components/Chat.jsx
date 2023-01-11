eb1e766edcf4c25eaa5ab0658c5a1004f78377f0
MishaKollen <mikhailkollen@gmail.com>
2023-01-11 16:19:14 +0400

    initial commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Cam from '../img/cam.png'
-import Add from '../img/add.png'
-import More from '../img/more.png'
-import Messages from './Messages'
-import Input from "./Input"
+import React, { useContext } from "react";
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
+
 const Chat = () => {
+  const { data } = useContext(ChatContext);
+
   return (
-    <div className='chat'>
+    <div className="chat">
       <div className="chat-info">
-        <span>Jane</span>
+        <span>{data.user?.displayName}</span>
         <div className="chat-icons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
           <img src={More} alt="" />
         </div>
       </div>
-      <Messages></Messages>
+      <Messages />
       <Input/>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import userEvent from '@testing-library/user-event'
+import React, { useEffect } from 'react'
+import { useRef } from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { ChatContext } from '../context/ChatContext'
 
-const Message = () => {
+const Message = ({message}) => {
+  const {currentUser} = useContext(AuthContext)
+  const {data} = useContext(ChatContext)
+  const ref = useRef();
+  useEffect(() => {
+    ref.current?.scrollIntoView({behavior:"smooth"})
+  
+  }, [message])
+  
   return (
-    <div className='message owner'>
+    <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"} `}>
       <div className="message-info">
-        <img src="https://images.pexels.com/photos/14792109/pexels-photo-14792109.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+        <img src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
         <span>just now</span>
       </div>
       <div className="message-content">
-        <p>hello</p>
-        {/* <img src="https://images.pexels.com/photos/14792109/pexels-photo-14792109.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" /> */}
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
   )
